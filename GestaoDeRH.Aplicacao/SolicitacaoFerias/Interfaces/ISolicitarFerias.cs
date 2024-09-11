194f77695af8c18dfb0a26d3ca1d1e9585d705fe
Fabis-li <fabiano_slima@hotmail.com>
2024-09-11 20:40:18 -0300

    criado interface na camada de aplicacao

---
+++ GestaoDeRH.Aplicacao/SolicitacaoFerias/Interfaces/ISolicitarFerias.cs
@@ -0,0 +1,13 @@
+﻿using GestaoDeRH.Aplicacao.Base;
+using GestaoDeRH.Aplicacao.Ferias.DTO;
+
+namespace GestaoDeRH.Aplicacao.Ferias.Interfaces;
+
+public interface ISolicitarFerias
+{
+    Task<ResultadoOperacao<SolicitarFeriasDto>> SolicitarFerias(SolicitarFeriasDto dto);
+    Task<SolicitarFeriasDto> ObterPorId(int id);
+    Task<ResultadoOperacao<SolicitarFeriasDto>> Editar(SolicitarFeriasDto dto);
+    Task<ResultadoOperacao<SolicitarFeriasDto>> Excluir(int id);
+    Task<List<SolicitarFeriasDto>> Listar();
+}
