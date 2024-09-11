7a0f5da78a84bf1bdafeb8952a4998b16eba9e81
Fabis-li <fabiano_slima@hotmail.com>
2024-09-11 20:38:39 -0300

    criado repositorio na camada infra

---
+++ GestaoDeRH.Infrastructure/Repositorios/SolicitacaoFeriasRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using GestaoDeRH.Dominio.Ferias;
+using GestaoDeRH.Dominio.Interfaces;
+using GestaoDeRH.Infra.BancoDeDados;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoDeRH.Infra.Repositorios;
+
+public class SolicitacaoFeriasRepositorio(GestaoDeRhDbContext dbContext) : RepositorioGenerico<SolicitarFerias>(dbContext),  IRepositorioSolicitarFerias
+{
+    public Task<SolicitarFerias?> ObterPorId(int id)
+    {
+        return dbContext.SolicitarFerias.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public Task<List<SolicitarFerias>> ListarFeriasPorPeriodo(DateTime dataInicio, DateTime dataFim)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<SolicitarFerias>> ListarFeriasPorColaborador(int colaboradorId)
+    {
+        throw new NotImplementedException();
+    }
+}
