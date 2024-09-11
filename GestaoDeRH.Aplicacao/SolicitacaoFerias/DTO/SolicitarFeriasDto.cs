b74d6a308be3abe12bfe156d564765781443025f
Fabis-li <fabiano_slima@hotmail.com>
2024-09-11 20:39:43 -0300

    criado dto

---
+++ GestaoDeRH.Aplicacao/SolicitacaoFerias/DTO/SolicitarFeriasDto.cs
@@ -0,0 +1,21 @@
+﻿namespace GestaoDeRH.Aplicacao.Ferias.DTO;
+
+public class SolicitarFeriasDto
+{
+    public int Id { get; set; }
+    public int ColaboradorId { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataFim { get; set; }
+    
+    public DateTime SolicitacaoDeFerias { get; set; }
+
+    public bool ValidarDuracaoFerias()
+    {
+        return DataFim.Subtract(DataInicio).Days <= 30;
+    }
+
+    public int CalcularDiasFerias()
+    {
+        return DataFim.Subtract(DataInicio).Days + 1;
+    }
+}
