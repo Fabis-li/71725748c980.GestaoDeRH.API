6a78390bbbcc537db1b5c001b2de451cc2607ad7
Fabis-li <fabiano_slima@hotmail.com>
2024-09-11 20:46:23 -0300

    criado migraçoes

---
+++ GestaoDeRH.Infrastructure/Migrations/20240911030736_Add Tabelas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoDeRH.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBFerias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ColaboradorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataInicioFerias = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFimFerias = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Solicitacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBFerias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBFerias_Colaboradores_ColaboradorId",
+                        column: x => x.ColaboradorId,
+                        principalTable: "Colaboradores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBFerias_ColaboradorId",
+                table: "TBFerias",
+                column: "ColaboradorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBFerias");
+        }
+    }
+}
---
+++ GestaoDeRH.Infrastructure/Migrations/20240911031726_Atualizar nome tabela ferias.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoDeRH.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Atualizarnometabelaferias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TBFerias_Colaboradores_ColaboradorId",
+                table: "TBFerias");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TBFerias",
+                table: "TBFerias");
+
+            migrationBuilder.RenameTable(
+                name: "TBFerias",
+                newName: "Ferias");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TBFerias_ColaboradorId",
+                table: "Ferias",
+                newName: "IX_Ferias_ColaboradorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ferias",
+                table: "Ferias",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ferias_Colaboradores_ColaboradorId",
+                table: "Ferias",
+                column: "ColaboradorId",
+                principalTable: "Colaboradores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ferias_Colaboradores_ColaboradorId",
+                table: "Ferias");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ferias",
+                table: "Ferias");
+
+            migrationBuilder.RenameTable(
+                name: "Ferias",
+                newName: "TBFerias");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ferias_ColaboradorId",
+                table: "TBFerias",
+                newName: "IX_TBFerias_ColaboradorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TBFerias",
+                table: "TBFerias",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TBFerias_Colaboradores_ColaboradorId",
+                table: "TBFerias",
+                column: "ColaboradorId",
+                principalTable: "Colaboradores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
