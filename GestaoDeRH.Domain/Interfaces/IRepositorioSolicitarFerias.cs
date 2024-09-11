58d0d6ea957662d5e92dfd3adeb60450398b320d
Fabis-li <fabiano_slima@hotmail.com>
2024-09-11 20:35:26 -0300

    criado interface repositorio

---
+++ GestaoDeRH.Domain/Interfaces/IRepositorioSolicitarFerias.cs
@@ -0,0 +1,10 @@
+﻿using GestaoDeRH.Dominio.Ferias;
+
+namespace GestaoDeRH.Dominio.Interfaces;
+
+public interface IRepositorioSolicitarFerias
+{
+    Task<SolicitarFerias> ObterPorId(int id);
+    Task<List<SolicitarFerias>> ListarFeriasPorPeriodo(DateTime dataInicio, DateTime dataFim);
+    Task<List<SolicitarFerias>> ListarFeriasPorColaborador(int colaboradorId);
+}
